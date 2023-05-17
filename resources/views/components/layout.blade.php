f259aa8d277a4ce74b8575c448053902883ecada
calvins-simbolon <thecalvins.simbolon@gmail.com>
2023-05-17 22:08:34 +0700

    Menambah Contact dan update Footer : Client Side DONE

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="scroll-smooth">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('asset/css/suplement.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
 
     {{-- This is Service Section --}}
     <div class="py-10 px-10" id="services">
-        <h1 class="font-bold text-4xl text-center">Layanan Kami</h1>
+        <h1 class="font-bold text-gray-900 text-4xl text-center">Layanan Kami</h1>
 
         {{-- Macam - macam layanan kami --}}
         <div class="grid md:grid-cols-4 gap-8 w-fit my-8 mx-auto">
@@ -40,6 +40,31 @@
     
     {{-- Service Section Done --}}
 
+    {{-- Contact Pages Start --}}
+    <div class="py-10 px-10 bg-slate-800">
+        <h1 class="font-bold text-gray-100 text-4xl text-center">Hubungi Kami</h1>
+        <div class="grid lg:flex gap-8 my-8 mx-auto">
+            <div class="gmaps-bang max-w-xl mx-auto lg:mx-0 border">
+                <iframe class="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1052.7794758650653!2d110.38432968187257!3d-6.994022402756271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b23d9765ca3%3A0xd64975d978ab0612!2sJl.%20Sri%20Rejeki%20Timur%20V%2C%20Gisikdrono%2C%20Kec.%20Semarang%20Barat%2C%20Kota%20Semarang%2C%20Jawa%20Tengah%2050149!5e0!3m2!1sid!2sid!4v1684332888864!5m2!1sid!2sid"  style="width: 100%; height: 100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            {{-- Identitas bg --}}
+            <div  class="grid gap-3 h-fit my-10 text-gray-300">
+                {{-- <h1>Phoenix Autogate System</h1> --}}
+                <h1 class="h-fit text-3xl font-bold text-gray-200">Agus Riyanto</h1>
+                <div >
+                    <p class="text-lg">Jl. Sri Rejeki Timur Raya, Semarang Barat</p>
+                    <p class="text-lg">Semarang - Indonesia</p>
+                </div>
+                <div class="grid gap-1">
+                    <a href="tel:+628170553367" target="_blank" class="flex gap-2"><i class="bi bi-telephone-fill"></i>08170553367</a>
+                    <a href="mailto: phoenix@gmail.com" target="_blank" class="flex gap-2"> <i class="bi bi-envelope-fill" ></i>phoenix@gmail.com</a>
+                </div>
 
-    <x-footer></x-footer>
+            </div>
+        </div>
+        <h1 class="text-center text-gray-400">©2023 Phoenix Autogate System</h1>
+    </div>
+    {{-- Contact Pages Ends --}}
+
+
 </x-layout>
